71bdf673227376a5c960dcb48ab8c0d377fd59c0
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-10-18 20:03:44 +0530

    frontend design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,32 @@
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-       New Project
-    </h1>
-  )
+    <>
+      <main className="max-w-xl mx-auto flex gap-4">
+        <div>
+          <h1 className="text-3xl font-bold underline">
+            Generate a video from a URL using power of AI
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
+            provident, molestias alias libero quas ipsum a, harum earum odit
+            voluptates consequatur! Et omnis perspiciatis tempora dolore
+            corporis ipsam tempore suscipit.
+          </p>
+          <form action="">
+            <input
+              type="
+        url"
+              placeholder="https://....."
+              className="border-black border-2"
+            />
+
+            <button type="submit">Create Video</button>
+          </form>
+        </div>
+        <div>
+          <div className="bg-gray-200 w-[240px] h-[380px]">a</div>
+        </div>
+      </main>
+    </>
+  );
 }
