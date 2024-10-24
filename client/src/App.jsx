7905ae5fa09143b5db4b1ca36c26ba23d9fc4569
soamtripathy <soumyaranjantripathy44@gmail.com>
2024-10-24 11:55:04 +0530

    landing page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,36 @@
 export default function App() {
   return (
     <>
-      <main className="max-w-xl mx-auto flex gap-4">
-        <div>
-          <h1 className="text-3xl font-bold underline">
-            Generate a video from a URL using power of AI
+      <main className="max-w-xl mx-auto flex px-4 gap-8">
+        <div className="py-8  flex flex-col justify-center">
+          <h1 className="text-4xl font-bold uppercase mb-4">
+            <span className="text-5xl "> URL to Video</span> <br />
+            <span className="bg-gradient-to-br from-emerald-300 from-30% to-sky-500 bg-clip-text text-transparent">
+              {" "}
+              using power of AI
+            </span>
           </h1>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur,
-            provident, molestias alias libero quas ipsum a, harum earum odit
-            voluptates consequatur! Et omnis perspiciatis tempora dolore
-            corporis ipsam tempore suscipit.
-          </p>
-          <form action="">
+
+          <form action="" className="  grid gap-2 ">
             <input
               type="
         url"
               placeholder="https://....."
-              className="border-black border-2"
+              className="bg-transparent text-white border-2 rounded-full px-4 py-2 grow"
             />
 
-            <button type="submit">Create Video</button>
+            <button
+              type="submit"
+              className=" bg-emerald-500 text-white px-4 py-2 rounded-full uppercase "
+            >
+              Create&nbsp;Video
+            </button>
           </form>
         </div>
-        <div>
-          <div className="bg-gray-200 w-[240px] h-[380px]">a</div>
+        <div className="py-4">
+          <div className="bg-gray-200 w-[240px] h-[380px] text-gray-500 rounded-2xl p-8">
+            Video Here
+          </div>
         </div>
       </main>
     </>
